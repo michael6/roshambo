f0bfbe76e819905eee3750860804b1f6415a1950
Junyi <michaelf614@gmail.com>
2012-03-07 18:34:57 -0800

    Update spec/controllers/pages_controller_spec.rb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,12 +15,28 @@ describe PagesController do
       get 'rock'
       response.should be_success
     end
+    it "should show computer_throw" do
+      get 'rock'
+      response.should have_selector("body", :content=>"rock"||"paper"||"lizard"||"scissors"||"spock")
+    end
+    it "should show result" do
+	    get 'rock'
+	    response.should have_selector("body", :content=>"Ouch"||"Nicely done"||"tied")
+    end
   end
 
    describe "GET 'paper'" do
     it "should be successful" do
       get 'paper'
       response.should be_success
+    end
+    it "should show computer_throw" do
+      get 'paper'
+      response.should have_selector("body", :content=>"rock"||"paper"||"lizard"||"scissors"||"spock")
+    end
+    it "should show result" do
+	    get 'paper'
+	    response.should have_selector("body", :content=>"Ouch"||"Nicely done"||"tied")
     end
   end
   
@@ -29,6 +45,15 @@ describe PagesController do
       get 'scissors'
       response.should be_success
     end
+    it "should show computer_throw" do
+      get 'scissors'
+      response.should have_selector("body", :content=>"rock"||"paper"||"lizard"||"scissors"||"spock")
+    end
+    it "should show result" do
+	    get 'scissors'
+	    response.should have_selector("body", :content=>"Ouch"||"Nicely done"||"tied")
+    end
+  end
   end
   
    describe "GET 'spock'" do
@@ -36,12 +61,28 @@ describe PagesController do
       get 'spock'
       response.should be_success
     end
+    it "should show computer_throw" do
+      get 'spock'
+      response.should have_selector("body", :content=>"rock"||"paper"||"lizard"||"scissors"||"spock")
+    end
+    it "should show result" do
+	    get 'spock'
+	    response.should have_selector("body", :content=>"Ouch"||"Nicely done"||"tied")
+    end
   end
   
    describe "GET 'lizard'" do
     it "should be successful" do
       get 'lizard'
       response.should be_success
+    end
+    it "should show computer_throw" do
+      get 'lizard'
+      response.should have_selector("body", :content=>"rock"||"paper"||"lizard"||"scissors"||"spock")
+    end
+    it "should show result" do
+	    get 'lizard'
+	    response.should have_selector("body", :content=>"Ouch"||"Nicely done"||"tied")
     end
   end
   
